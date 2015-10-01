1d7e1681a144e24d7cb8f0caabaf6df7384453ca
Kevin TianXiang Wu <kevintxwu@gmail.com>
2015-09-30 20:24:24 -0700

    fix everything except for squah

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
 
 end
