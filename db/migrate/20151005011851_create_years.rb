dfa15c899486ad7a6cc466ac1415fd88ef4f5956
JonChu <jonmchu@berkeley.edu>
2015-10-04 18:22:05 -0700

    created Year model.

---
+++ db/migrate/20151005011851_create_years.rb
@@ -0,0 +1,10 @@
+class CreateYears < ActiveRecord::Migration
+  def change
+    create_table :years do |t|
+      t.integer :date
+      t.integer :attribute
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001010023) do
+ActiveRecord::Schema.define(version: 20151005011851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20151001010023) do
     t.date     "earnings_begin"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "years", force: :cascade do |t|
+    t.integer  "date"
+    t.integer  "attribute"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
