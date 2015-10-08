4db463ac376f56e9219643f588966c4fa53a0f59
Kevin TianXiang Wu <kevintxwu@gmail.com>
2015-10-08 03:26:41 -0700

    starting to find basic style

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/index', to: 'static_pages#index'
+  get '/project', to: 'static_pages#project'
+  get '/information', to: 'static_pages#information'
 
 end
