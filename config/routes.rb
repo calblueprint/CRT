24c1c82958d0587961a1856641647679932682c6
Kevin TianXiang Wu <kevintxwu@gmail.com>
2015-10-10 15:22:21 -0700

    mockup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   resources :projects
 
-  root 'projects#index'
-  #root 'static_pages#home'
+  # root 'projects#index'
+  root 'static_pages#home'
   
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
