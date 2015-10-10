f54ceeac176d7e6b83cc49ae75c2b8f49fe92d60
Kevin TianXiang Wu <kevintxwu@gmail.com>
2015-10-10 15:12:11 -0700

    donezo

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/index', to: 'static_pages#index'
+  get '/new-project', to: 'static_pages#new-project'
   get '/project', to: 'static_pages#project'
+  get '/project-details', to: 'static_pages#project-details'
+  get '/project-attributes', to: 'static_pages#project-attributes'
   get '/attributes', to: 'static_pages#attributes'
+  get '/new-attribute', to: 'static_pages#new-attribute'
+  get '/rearrange-attributes', to: 'static_pages#rearrange-attributes'
+  get '/edit-attribute', to: 'static_pages#edit-attribute'
   get '/information', to: 'static_pages#information'
 
 end
