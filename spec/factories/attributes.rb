2258ef4cd91d20f4c8883a03a43c7c9eb98a5d34
Sam Lau <samlau95@gmail.com>
2015-10-12 18:53:11 -0700

    Cleanup indentation and annotate models
    I actually like making an alias that runs
    `rake db:migrate && annotate` so that models are always annotated.

--- spec/factories/attributes.rb
+++ spec/factories/attributes.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: attributes
+#
+#  id         :integer          not null, primary key
+#  type       :integer
+#  value      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  year_id    :integer
+#
+
 FactoryGirl.define do
   factory :attribute do
     type 1
--- spec/factories/years.rb
+++ spec/factories/years.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: years
+#
+#  id         :integer          not null, primary key
+#  date       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :year do
     date 1
