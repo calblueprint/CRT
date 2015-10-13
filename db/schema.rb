7b4138d72fae90463e2c369dd55cdb48cbae5a7c
JonChu <jonmchu@berkeley.edu>
2015-10-12 20:35:30 -0700

    deleted formula table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013031345) do
+ActiveRecord::Schema.define(version: 20151013033430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 20151013031345) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "formulas", force: :cascade do |t|
-    t.string   "formula"
-    t.integer  "attribute_type"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
   end
 
   create_table "projects", force: :cascade do |t|
