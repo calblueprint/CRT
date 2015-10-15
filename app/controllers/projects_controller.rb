e288605c8e760231f27c8792b9ee56afacad224d
Isaiah Yoon <isaiahyoon@berkeley.edu>
2015-10-14 18:44:58 -0700

    Add create, update, destroy for Project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,49 @@
 class ProjectsController < ApplicationController
-    def index
-        @projects = Project.all
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @project = Project.find(params[:id])
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
     end
+  end
 
-    def new
-    end
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
 
-    def create
-        @project = Project.new(params[:article])
+    redirect_to projects_path
+  end
 
-        @project.save
-        redirect_to @project
+  private
+    def project_params
+      params.require(:project).permit(:name, :acres, :date_closed, :restricted_endowment, :cap_rate, :admin_rate, :total_upfront, :years_upfront, :earnings_begin)
     end
 end
