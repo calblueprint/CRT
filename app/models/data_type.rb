328013a57345339c95d8b752a76a58d93836a6f9
Jordeen Chang <jordeenchang@gmail.com>
2015-10-18 18:58:48 -0700

    fixing model files for data types and data values

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,2 +1,13 @@
+
+# == Schema Information
+#
+# Table name: data_types
+#
+#  id           :integer          not null, primary key
+#  formula      :string           not null
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+
 class DataType < ActiveRecord::Base
 end
---
+++ app/models/data_value.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: data_values
+#
+#  id           :integer          not null, primary key
+#  type         :integer
+#  value        :decimal(, )
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  year_id      :integer          not null
+#  data_type_id :integer          not null
+
+class DataValue < ActiveRecord::Base
+end
