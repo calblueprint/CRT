3af9f69197615b144c63636a467a4853594088c6
Isaiah Yoon <isaiahyoon@berkeley.edu>
2015-10-21 19:02:10 -0700

    Add year to all proj on create (#25)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   resources :projects
+  resources :years
 
   # root 'projects#index'
   root 'static_pages#home'
-  
+
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/index', to: 'static_pages#index'
---
+++ spec/controllers/years_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe YearsController, :type => :controller do
+
+end
