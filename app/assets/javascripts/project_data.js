a36c7f04f0caf3010803f86f6c6177fb7e9d1373
Andy Qin <andrewlqin@gmail.com>
2016-03-12 16:48:30 -0800

    Create frontend for adding input_formula's
    Summary:
    You can now add formulae directly into the cells of a Project. Should
    work for Universal Access. Known issues include lack of handling
    mistyped formulae.
    
    Test Plan: Enter formulae into Project cells and watch them render.
    
    Reviewers: jsonderulo
    
    Reviewed By: jsonderulo
    
    Subscribers: jonchuchu, quinton, isaiahyoon, qin
    
    Projects: #crt
    
    Differential Revision: http://phab.calblueprint.org/D359

--- app/assets/javascripts/project_data.js
+++ app/assets/javascripts/project_data.js
@@ -6,28 +6,42 @@ $(document).ready(function() {
     var id = $(this).attr('data-id');
     $(this).data('startVal', startVal);
     $(this).data('id', id);
+
+    // populate cell with input formula if one exists
+    var inputFormula = $(this).attr('data-input-formula');
+    $(this).html(inputFormula);
   }).blur(function() {
     var newVal = $(this).html();
+
     if ($(this).data('startVal') != newVal) {
-      var data_id = $(this).data('id')
-      $.ajax('/data_values/' + data_id, {method: 'PATCH', data: {data_value: {value: newVal}}, success: handleData, error: handleError})
+      var data_id = $(this).data('id');
+      $.ajax('/data_values/' + data_id, {method: 'PATCH', data: {data_value: {input_value: newVal}}, success: handleData, error: handleError}); 
     }
   });
 
   // handle update for all data values after edit
   function handleData(data) {
     var data_values = data["data_values"]
-    for (var i=0; i < data_values.length; i++) {
+    var showSuccess = true
+    for (var i = 0; i < data_values.length; i++) {
       var dataAttrString = '[data-id="'+ data_values[i].id +'"]';
       var dataVal = data_values[i].value;
-
       if (isCustomData(dataVal)) {
         $(dataAttrString).html(dataVal);
       } else {
         $(dataAttrString).html(data_values[i].formula_value);
       }
+      // update cell with input formula if one exists
+      var oldFormula = $(dataAttrString).attr('data-input-formula');
+      var newFormula = data_values[i].input_formula
+      if (oldFormula == newFormula) {
+        showSuccess = false
+      }
+      $(dataAttrString).attr('data-input-formula', newFormula);
     }
-    complete();
+    if (showSuccess) {
+      complete();
+    }
   }
 
   // Checks if there is a custom entered value or not. Returns true if custom value.
--- app/controllers/data_values_controller.rb
+++ app/controllers/data_values_controller.rb
@@ -24,7 +24,7 @@ class DataValuesController < ApplicationController
 
   def update
     @data_value = DataValue.find(params[:id])
-    @data_value.update(data_value_params)
+    @data_value.update(parse_input_value_params)
     @project = Project.find(@data_value.project)
     @data_types = DataType.where(master: @project.master?).order(:order)
     ParseFormulaService.update_data_values(@project, @data_types)
@@ -34,6 +34,17 @@ class DataValuesController < ApplicationController
   private
 
   def data_value_params
-    params.require(:data_value).permit(:value, :data_type_id)
+    params.require(:data_value).permit(:value, :input_formula, :data_type_id)
+  end
+
+  def parse_input_value_params
+    input_value_string = params[:data_value][:input_value]
+    input_value = Float(input_value_string) rescue input_value_string
+    if input_value.is_a? String
+      parse_input_value_params = { value: nil, input_formula: input_value }
+    else
+      parse_input_value_params = { value: input_value, input_formula: nil }
+    end
+    parse_input_value_params
   end
 end
