0a24f5f957699f9e37e482b9a49de7da4a7c3b57
Isaiah Yoon <isaiahyoon@berkeley.edu>
2016-04-11 17:14:23 -0700

    Testing plan for index view
    Summary:
    Show master first, show all projects as well
    <longer, detailed summary>
    
    Test Plan: Run test!
    
    Reviewers: jsonderulo
    
    Reviewed By: jsonderulo
    
    Subscribers: qin, quinton, jonchuchu
    
    Projects: #crt
    
    Differential Revision: http://phab.calblueprint.org/D448

---
+++ spec/features/projects_index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature "Index shows all projects, master first", type: :feature do
+  scenario "User creates a new non-master project" do
+    visit new_user_session_path
+    fill_in "E-mail", with: "admin@admin.com"
+    fill_in "Password", with: "password"
+    click_button "Sign In"
+
+    click_link "New Project"
+
+    fill_in 'Name', with: "Test project 2"
+    fill_in 'Acres', with: 350
+    select '2012', from: "project[date_closed(1i)]"
+    select 'January', from: "project[date_closed(2i)]"
+    select '1', from: "project[date_closed(3i)]"
+    fill_in 'Restricted endowment', with: 10
+    fill_in 'Cap rate', with: 20
+    fill_in 'Admin rate', with: 30
+    fill_in 'Total upfront', with: 40
+    fill_in 'Years upfront', with: 5
+    select '2016', from: "project[earnings_begin(1i)]"
+    select 'January', from: "project[earnings_begin(2i)]"
+    select '1', from: "project[earnings_begin(3i)]"
+
+    click_button "Create Project"
+    click_link "Back"
+
+    expect(page).to have_content("Test project 1") # admin
+    expect(page).to have_content("Test project 2")
+  end
+end
