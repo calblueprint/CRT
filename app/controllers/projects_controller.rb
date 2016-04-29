4f3a02f24ca6be81364a4860c0830ad2d689021d
JonChu <jonmchu@berkeley.edu>
2016-04-29 01:01:09 -0700

    add more rspec tests for data values. fixed project edit bug.
    Summary: Fixed bug on project editing when edits that do not pass validation redirect to bad url. Added tests to test for data value formula.
    
    Test Plan: Go to Project edit page and click edit project. Enter an invalid value in acres such as a negative number and click update project. Should redirect to edit project page with error messages next to related input boxes.
    
    Reviewers: jodreen, jsonderulo
    
    Reviewed By: jsonderulo
    
    Subscribers: kwu, isaiahyoon
    
    Projects: #crt
    
    Differential Revision: http://phab.calblueprint.org/D578

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -75,7 +75,7 @@ class ProjectsController < ApplicationController
     if @project.update(project_params)
       redirect_to @project
     else
-      redirect_to 'show'
+      render 'edit'
     end
   end
 
--- spec/models/data_value_spec.rb
+++ spec/models/data_value_spec.rb
@@ -4,12 +4,12 @@
 #
 #  id              :integer          not null, primary key
 #  type            :integer
-#  value           :decimal(, )
+#  value           :decimal(19, 4)
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  data_type_id    :integer          not null
 #  project_year_id :integer          not null
-#  formula_value   :decimal(, )
+#  formula_value   :decimal(19, 4)
 #  input_formula   :string
 #
 # Indexes
@@ -18,9 +18,10 @@
 #
 
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe DataValue, type: :model do
-  let(:data_value) { create :data_value, value: 10.0 }
+  let(:data_value) { create :data_value }
   let(:data_value_formula) { create :data_value, input_formula: "1 + 3" }
 
   describe '#create' do
@@ -28,6 +29,18 @@ RSpec.describe DataValue, type: :model do
       new_data_value = build :data_value
       new_data_value.data_type_id = nil
       expect(new_data_value).to_not be_valid
+    end
+  end
+
+  describe '#formula' do
+    it 'should have same formula as data type' do
+      dt = data_value.data_type
+      expect(data_value.formula).to eq(dt.formula)
+    end
+
+    it 'should have different formula as data type' do
+      dt = data_value_formula.data_type
+      expect(data_value_formula.formula).to_not eq(dt.formula)
     end
   end
 end
